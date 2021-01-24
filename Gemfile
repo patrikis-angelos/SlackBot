bcf0ba227783ef2997c7cf6b72328cb911e2ed4f
Patrick Angelos <angelosp07@gmail.com>
2021-01-24 21:28:15 +0200

    Add basic bot functionality

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'dotenv'
+gem 'async-websocket', '~> 0.8.0'
+gem 'slack-ruby-bot'
--- main.rb
+++ main.rb
@@ -1,33 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'net/http'
-require 'json'
 require 'dotenv'
+require 'slack-ruby-bot'
 Dotenv.load
 
-postSomething = URI "https://slack.com/api/chat.postMessage"
-params = {
-    token: ENV['SLACK_BOT_TOKEN'],
-    channel: '#general',
-    text: 'Hello'
-}
-postSomething.query = URI.encode_www_form(params)
-res = Net::HTTP.get_response(postSomething)
-
-puts JSON.pretty_generate(JSON.parse(res.body))
-
-uri = URI.parse("https://slack.com/api/apps.connections.open")
-request = Net::HTTP::Post.new(uri)
-request.content_type = "application/x-www-form-urlencoded"
-request["Authorization"] = "Bearer #{ENV['SLACK_APP_TOKEN']}"
-
-req_options = {
-  use_ssl: uri.scheme == "https",
-}
-
-response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-  http.request(request)
+def take_action (action, params)
+    action = URI "https://slack.com/api/#{action}"
+    action.query = URI.encode_www_form(params)
+    response = Net::HTTP.get_response(action)
 end
 
-response = JSON.parse(response.body)
-puts response['url']
+class Bot < SlackRubyBot::Bot
+    command 'hi' do |_client, data, _match|
+        take_action('chat.postMessage', {token: ENV['SLACK_API_TOKEN'], channel: data.channel, text:'Hello'})
+    end
+end
+
+Bot.run
